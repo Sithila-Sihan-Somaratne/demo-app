0f07feae523ea1f5d3c30df23b2bff4cb7441064
Sithila2010 <chutimalli182@gmail.com>
2023-09-23 07:02:07 +0200

    Angular Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { RegisterComponent } from './page/register/register.component';
+import { SuccessComponent } from './page/success/success.component';
+import { InitialComponent } from './page/initial/initial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    SuccessComponent,
+    InitialComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page/initial/initial.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-initial',
+  templateUrl: './initial.component.html',
+  styleUrls: ['./initial.component.css']
+})
+export class InitialComponent {
+
+}
